91f184c0bfcc101b4c96943b08a843c32894da19
ES <schahalinka@icloud.com>
2022-09-18 22:20:00 +0200

    HomeWork-2.30

--- HoweWorkTrafficLight/ViewController.swift
+++ HoweWorkTrafficLight/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    var counter = 0
+    @IBOutlet var viewTrafficLightRed: UIView!
+    
+    @IBOutlet var viewTrafficLightYellow: UIView!
+    
+    @IBOutlet var viewTrafficLightGreen: UIView!
+    
+    @IBOutlet var startingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startingButton.layer.cornerRadius = 10
+        viewTrafficLightRed.layer.cornerRadius = viewTrafficLightRed.frame.width / 2
+        viewTrafficLightYellow.layer.cornerRadius = viewTrafficLightYellow.frame.width / 2
+        viewTrafficLightGreen.layer.cornerRadius = viewTrafficLightGreen.frame.width / 2
+        viewTrafficLightRed.alpha = 0.3
+        viewTrafficLightYellow.alpha = 0.3
+        viewTrafficLightGreen.alpha = 0.3
     }
-
-
+    
+    @IBAction func startingButtonDidTapped() {
+        startingButton.setTitle(
+            startingButton.isHidden ? "START" : "NEXT",
+            for: .normal
+        )
+        switch counter {
+            case 0:
+                viewTrafficLightRed.alpha = 1
+                viewTrafficLightYellow.alpha = 0.3
+                viewTrafficLightGreen.alpha = 0.3
+            case 1:
+                viewTrafficLightRed.alpha = 0.3
+                viewTrafficLightYellow.alpha = 1
+                viewTrafficLightGreen.alpha = 0.3
+            default:
+                viewTrafficLightRed.alpha = 0.3
+                viewTrafficLightYellow.alpha = 0.3
+                viewTrafficLightGreen.alpha = 1
+        }
+        counter += 1
+        if counter == 3 {
+            counter = 0
+        }
+    }
 }
 
